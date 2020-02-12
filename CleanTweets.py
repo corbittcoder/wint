0bf610093bc6d9c0b9e1b5ce99d431cad7444413
Scott A. Corbitt <scott.a.corbitt@gmail.com>
2020-02-12 01:37:02 -0700

    Adding script to clean tweets

--- CleanTweets.py
+++ CleanTweets.py
@@ -1,12 +1,26 @@
-quotes = open(r"C:\Users\scttc\AppData\Local\Programs\Python\Python36-32\TrumpTweets.txt", encoding='ISO-8859-1')
+quotes = open(r"C:\Users\scttc\AppData\Local\Programs\Python\Python36-32\TrumpTweets.txt", encoding='utf-8')
 contents = quotes.read()
-print(contents)
+# print(contents)
 quotes.close()
 
 ##eliminate all URLS in tweets
 import pandas as pd
-
-print(contents.split('\n')[1])
-df = pd.DataFrame(contents.split('\n'))
+import re
+filtered = re.sub(r'^https?:\/\/.*[\r\n]*', '', contents, flags=re.MULTILINE)
+df = pd.DataFrame(filtered.split('\n'), columns=['text'])
+delete = 'http'
+remove = df['text'].str.contains(delete)
+df = df[~remove]
 print(df.size)
+# print(df[1:10])
+
+#write to file
+series = pd.Series(df['text'])
+fname = "C:\\Users\\scttc\\PycharmProjects\\wint\\TrumpFilteredTweets.txt"
+f = open(fname, 'a')
+output = series.str.cat(sep='\n')
+with open(fname, "w", encoding="utf-8") as f:
+    f.write(output)
+f.close()
+# print(series.str.cat(sep='\n'))
 
